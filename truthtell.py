70f1579ca144294aa33b8c31ab3970e36fd299d6
Sai Tharun Aditya <saitarunaditya@gmail.com>
2024-12-14 16:12:51 +0530

    modified truthtell.py and added liar.py

--- truthtell.py
+++ truthtell.py
@@ -1,4 +1,8 @@
-import nltk
-sentence = "Allu arjun arrested"
-words = nltk.word_tokenize(sentence)
-print(words)
+from datasets import load_dataset
+
+# Load the dataset
+dataset = load_dataset("liar.py", split="train")
+
+# Inspect the dataset
+print(dataset.num_rows)  # Print the first data sample
+
