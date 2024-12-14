5fc52b7fcaf3ca0297ced62f201f9af8e5b79f09
Sai Tharun Aditya <saitarunaditya@gmail.com>
2024-12-14 16:42:54 +0530

    added liar datset, preprocessed dataset and added preprocessed dataset

--- truthtell.py
+++ truthtell.py
@@ -1,8 +1,28 @@
-from datasets import load_dataset
+import pandas as pd
+
+column_names = [
+    "id", "label", "statement", "subject", "speaker", "job_title", 
+    "state_info", "party_affiliation", "barely_true_counts", "false_counts", 
+    "half_true_counts", "mostly_true_counts", "pants_on_fire_counts", "context"
+]
 
 # Load the dataset
-dataset = load_dataset("liar.py", split="train")
+data = pd.read_csv("/Users/saitarunaditya/Desktop/TruthGuard/liar_dataset/train.csv", header=None, names=column_names)
 
-# Inspect the dataset
-print(dataset.num_rows)  # Print the first data sample
+# Preview the dataset
+print(data.head())
+print(data.info())
 
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+
+
+# Clean text
+data['cleaned_text'] = data['statement'].str.replace(r'\W', ' ').str.lower()
+
+# Tokenize
+data['tokens'] = data['cleaned_text'].apply(word_tokenize)
+
+# Save preprocessed data
+data.to_csv("preprocessed_data.csv", index=False)
